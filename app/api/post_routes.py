17094d32081935cbdcbbce74f53e160762655deb
Ryan <ryanjerickson@gmail.com>
2023-08-08 16:01:45 -0500

    Get Routes Complete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -11,4 +11,20 @@ def posts():
     Query for all posts
     """
     posts = Post.query.all()
+
+    if not posts:
+        return {'message': 'no posts found'}
+
     return {'posts': [post.to_dict() for post in posts]}
+
+@post_routes.route('/<int:id>')
+def post(id):
+    """
+    Query for single post
+    """
+    post = Post.query.get(id)
+
+    if not post:
+        return {'message': 'post not found'}
+
+    return post.to_dict()
