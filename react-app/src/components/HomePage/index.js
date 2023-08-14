bd64d2b6d4208ec0ef4e4eefe738655a5daaf5e1
Ryan <ryanjerickson@gmail.com>
2023-08-14 11:56:41 -0500

    CSS"

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -24,14 +24,21 @@ function HomePage() {
         }
     }, [dispatch])
 
+    const handlePost = () => {
+        history.push('/posts/new')
+    }
+
     if (!loaded) return <h2>Loading...</h2>
 
     return (
         <div className="main-display-container">
             <div className="post-main-container">
-                <div className="post-container">
-                    <input placeholder="Create Post"></input>
-                </div>
+                {sessionUser ? <div className="post-container">
+                    <div className='post-input-container'>
+                        <img id='post-profile-pic' src={sessionUser?.profile_pic} alt='User Profile Pic' />
+                        <input id='create-post-input' onClick={handlePost} placeholder="Create Post"></input>
+                    </div>
+                </div> : null}
                 {allPosts.posts.map(post => {
                     if (sessionUser?.id !== post.owner.id) {
                         return (
@@ -49,13 +56,13 @@ function HomePage() {
                     </div>
                 </div>
                 <div className="home-info-container">
-                    <img id='home-earth' src='/earth.png' alt='earth'/>
+                    <img id='home-earth' src='/earth.png' alt='earth' />
                     <div className="home-info-contents">
                         <p>Posterra Home</p>
                         <p>Posts from around the world...</p>
                         <p>Your personal homepage. Come here to check in with your favorite communities.</p>
-                        <button id='createpost' onClick={() => history.push('/posts/new')}>Create post</button><br />
-                        <button id='createcomm' onClick={() => history.push('/communities/new')}>Create community</button>
+                        {sessionUser ? <button id='createpost' onClick={() => history.push('/posts/new')}>Create post</button> : null}
+                        {sessionUser ? <button id='createcomm' onClick={() => history.push('/communities/new')}>Create community</button> : null}
                     </div>
                 </div>
             </div>
