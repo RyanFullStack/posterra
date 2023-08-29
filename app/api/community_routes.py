0c690f09a998135b13d1db79d563c901c1cfdd11
Ryan <ryanjerickson@gmail.com>
2023-08-29 12:25:12 -0500

    Removed Comment

--- app/api/community_routes.py
+++ app/api/community_routes.py
@@ -32,9 +32,7 @@ def community_posts(id):
 
     posts = community.posts
 
-    print('*********', sort)
-
-    if sort == 'best' or not sort:
+      if sort == 'best' or not sort:
         sorted_posts = sorted(posts, key=lambda post: post.to_dict()['numvotes'], reverse=True)
     if sort == 'popular':
         sorted_posts = sorted(posts, key=lambda post: post.to_dict()['numcomments'], reverse=True)
