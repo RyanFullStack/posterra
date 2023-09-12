573d5d8aa82079d4c570eb7aa015a77d3b6a3e11
Ryan <ryanjerickson@gmail.com>
2023-09-12 14:33:21 -0500

    Post Store

--- react-app/src/components/PostInfo/index.js
+++ react-app/src/components/PostInfo/index.js
@@ -1,5 +1,36 @@
-function PostInfo() {
+import { useParams, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { thunkGetSingleCommunity } from "../../store/community";
+import { thunkGetCommunityPosts, thunkGetSinglePost } from "../../store/post";
+import PostContainer from "../Post";
+import CommunityInfo from "../CommunityInfo";
+import Loading from "../Loading";
 
+function PostInfo() {
+    const { postId } = useParams()
+    const [loaded, setLoaded] = useState(false)
+    const [found, setFound] = useState(false)
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const data = async() => {
+            const res = await dispatch(thunkGetSinglePost(postId))
+            setLoaded(true)
+            if (res.id) {
+            setFound(true)
+            }
+        }
+        data()
+
+        return function() {
+            setLoaded(false)
+            setFound(false)
+        }
+    }, [dispatch])
+
+    if (!loaded) return <Loading />
+    if (!found) return <h2><center>Post not found!</center></h2>
 
     return (
         'POST'
