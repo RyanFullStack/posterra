97325e515a62594c8edb002d87fe84b9d8ec7b22
Ryan <ryanjerickson@gmail.com>
2024-02-12 14:11:46 -0600

    navbar mobile styling

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -45,7 +45,7 @@ function Navigation({ isLoaded }) {
 				<img id='logo' src='/logo.png' alt='Posterra Logo' />
 			</NavLink>
 
-			<div>
+			<div id='select-community-div'>
 				<select id='select' onChange={changeCommunity} value={selectedCommunity}>
 					<option value="none" disabled hidden>
 						Choose a community...
@@ -58,7 +58,7 @@ function Navigation({ isLoaded }) {
 				</select>
 			</div>
 
-			<div>
+			<div className='log-in-area'>
 
 				{isLoaded && sessionUser &&
 				<div className='log-in-container'><i onClick={handlePost} id='plus-post' title='Create Post' className="fa-solid fa-plus"></i>
